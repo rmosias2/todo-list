d0ae9df20b322ecfab1a00dacb09c9aafe509693
Rafael Mosias <rafael.mosias@iclinic.com.br>
2021-11-03 02:59:27 -0300

    feat: add header and inputTask on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
-import type { NextPage } from 'next'
+import { Box } from "@chakra-ui/react";
+import type { NextPage } from 'next';
+
 import ColorModeSwitcher from '../components/ToggleTheme';
+import Header from '../components/Header';
+import InputTask from '../components/InputTask';
 
 const Home: NextPage = () => {
-  return (<ColorModeSwitcher />)
+  return (
+    <Box mt={8} w="100%" maxW="600px" mx={"auto"}>
+        <ColorModeSwitcher />
+        <Header />
+        <InputTask />
+    </Box>
+  )
 }
 
 export default Home
