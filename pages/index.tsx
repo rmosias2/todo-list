0c38a1fdd0a7f8a86a8847f7440488ce7dcc08bf
Rafael Mosias <rafael.mosias@iclinic.com.br>
2021-11-03 15:11:21 -0300

    chore: import components to the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 import type { NextPage } from 'next';
 
 import ColorModeSwitcher from '../components/ToggleTheme';
 import Header from '../components/Header';
-import InputTask from '../components/InputTask';
+import InputSearch from '../components/InputSearch';
+import TaskList from "../components/TaskList";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
     <Box mt={8} w="100%" maxW="600px" mx={"auto"}>
-        <ColorModeSwitcher />
         <Header />
-        <InputTask />
+        <InputSearch />
+        <TaskList />
+        <Footer />
+        <Center>
+            <ColorModeSwitcher />
+        </Center>
     </Box>
   )
 }
