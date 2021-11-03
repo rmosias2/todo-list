991a308a2724c3fa8bd8a68cbb8be42f4b618464
Rafael Mosias <rafael.mosias@iclinic.com.br>
2021-11-03 15:10:41 -0300

    chore: add filter task state and localstorage

--- hooks/task.tsx
+++ hooks/task.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, { useState, useContext, createContext, useEffect } from 'react';
 import { v4 as uuid_v4 } from "uuid";
 
 interface Task {
@@ -9,25 +9,122 @@ interface Task {
 
 interface TaskContextData {
   tasks: Task[];
-  addTask(task: string): void;
+  tasksFiltered: Task[];
+  radioFilter: string;
+  inputTask: string;
+  addTask(taskName: string): void;
+  deleteTask(taskID: string): void;
+  completedTask(taskID: Task): void;
+  editTask(taskID: Task, taskName: string): void;
+  filterTask(taskName: string, filter: string): void;
 }
 
 const TaskContext = createContext<TaskContextData>({} as TaskContextData);
+const ISSERVER = typeof window === "undefined";
 
 export const TaskProvider: React.FC = ({ children }) => {
   const [tasks, setTask] = useState<Task[]>([]);
+  const [radioFilter, setRadioFilter] = useState<string>("all");
+  const [inputTask, setInputTask] = useState<string>("");
+  const [tasksFiltered, setTaskFiltered] = useState<Task[]>([]);
 
-  const addTask = (task: string) => {
+  useEffect(() => {
+    if (ISSERVER) return;
+    const localtasks = localStorage.getItem("@TodoApp:tasks");
+    if (localtasks) {
+        setTask(JSON.parse(localtasks));
+    }
+  }, [])
+
+  const addTask = (taskName: string) => {
       setTask([...tasks, {
-        name: task,
+        name: taskName,
         isCompleted: false,
         id: uuid_v4()
       }])
+      localStorage.setItem("@TodoApp:tasks", JSON.stringify(
+        [...tasks, {
+            name: taskName,
+            isCompleted: false,
+            id: uuid_v4()
+        }]));
+  }
+
+  const deleteTask = (taskID: string) => {
+    if (filterTask.length > 0) {
+        const newTaskArray = tasksFiltered.filter((task) => task.id !== taskID);
+        setTaskFiltered(newTaskArray);
+    }
+    const newTaskArray = tasks.filter((task) => task.id !== taskID);
+    setTask(newTaskArray);
+    localStorage.setItem("@TodoApp:tasks", JSON.stringify(newTaskArray));
+  }
+
+  const completedTask = (TaskObj: Task) => {
+    if (filterTask.length > 0) {
+        const newTaskArray =  tasksFiltered.map((task) =>
+            task.id !== TaskObj.id
+            ? task
+            : { ...task, isCompleted: !TaskObj.isCompleted }
+        )
+
+        setTaskFiltered(newTaskArray);
+    }
+
+    const newTaskArray =  tasks.map((task) =>
+        task.id !== TaskObj.id
+        ? task
+        : { ...task, isCompleted: !TaskObj.isCompleted }
+    )
+    setTask(newTaskArray);
+    localStorage.setItem("@TodoApp:tasks", JSON.stringify(newTaskArray));
+  }
+
+  const editTask = (TaskObj: Task, taskName: string) => {
+    const objIndex = tasks.findIndex((task => task.id == TaskObj.id));
+    tasks[objIndex].name = taskName;
+    
+    localStorage.setItem("@TodoApp:tasks", JSON.stringify(tasks));
+  }
+
+  const filterTask = (taskName: string, filter: string) => {
+    setRadioFilter(filter);
+    setInputTask(taskName);
+
+    if (filter === "all" && taskName) {
+        const newTaskFiltered = tasks.filter((task) => task.name.includes(taskName));
+        setTaskFiltered(newTaskFiltered);
+        return;
+    } 
+
+    if (filter === "complete" && taskName === "") {
+        const newTaskFiltered = tasks.filter((task) => task.isCompleted);
+        setTaskFiltered(newTaskFiltered);
+        return;
+    } 
+
+    if (filter === "complete" && taskName) {
+        const newTaskFiltered = tasks.filter((task) => task.isCompleted && task.name.includes(taskName));
+        setTaskFiltered(newTaskFiltered);
+        return;
+    } 
+    if (filter === "incomplete" && taskName) {
+        const newTaskFiltered = tasks.filter((task) => !task.isCompleted && task.name.includes(taskName));
+        setTaskFiltered(newTaskFiltered);
+        return;
+    }
+
+    if (filter === "incomplete" && taskName === "") {
+        const newTaskFiltered = tasks.filter((task) => !task.isCompleted);
+        setTaskFiltered(newTaskFiltered);
+        return;
+    }
+    setTaskFiltered([]);
   }
 
   return (
     <TaskContext.Provider
-      value={{ tasks: tasks, addTask }}
+      value={{ tasksFiltered, tasks: tasks, addTask, deleteTask, completedTask, editTask, filterTask, radioFilter, inputTask }}
     >
       {children}
     </TaskContext.Provider>
