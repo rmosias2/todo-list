acc2dbdb9f697c4ee16b9b0df6edfcb85ad14751
Rafael Mosias <rafael.mosias@iclinic.com.br>
2021-11-03 15:09:09 -0300

    style: centralize header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-      <Heading cursor="" mt={8} mb={3} as="h2" size="3xl" data-testid="header-todo-app">
+      <Heading cursor="" mt={8} mb={3} as="h2" size="3xl" data-testid="header-todo-app" textAlign="center">
         Todo App
       </Heading>
   );
